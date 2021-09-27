0d4f32c4ba6618aa38e4cc1a7a8f0c0b14bf63e7
Djisu <pfleischer2002@yahoo.co.uk>
2021-09-27 21:51:43 +0000

    publish to heroku

---
+++ backend/config/keys.js
@@ -0,0 +1,18 @@
+export const keys = {
+  mongoURI:
+    'mongodb+srv://church18:Timbuk2tudjesu@cluster0.nlxec.mongodb.net/church18?retryWrites=true&w=majority',
+  //'mongodb://localhost/worksite',
+  secretOrKey: 'secret',
+
+  user: 'djesudjoleto@gmail.com',
+  pass: 'Timbuk2tudjesu',
+}
+
+/* export const keys = {
+  mongoURI: 'your mongodb uri',
+  //'mongodb://localhost/worksite',
+  secretOrKey: 'secret',
+
+  user: 'your user email',
+  pass: 'your password',
+} */
